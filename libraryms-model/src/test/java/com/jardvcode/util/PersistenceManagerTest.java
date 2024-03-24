4e8e4d15f8876409c17648747086aa9d1a0f6a1c
jalexisrdv <jalexisrdv@outlook.com>
2024-03-23 23:36:16 -0600

    test implementation for persistence manager

---
+++ libraryms-model/src/test/java/com/jardvcode/util/PersistenceManagerTest.java
@@ -0,0 +1,25 @@
+package com.jardvcode.util;
+
+import static org.junit.Assert.*;
+
+import javax.persistence.EntityManager;
+
+import org.junit.Test;
+
+import com.jardvcode.model.util.PersistenceManager;
+
+public class PersistenceManagerTest {
+
+	@Test
+	public void persistence_manager_should_create_entity_manager_factory() {
+		try {
+			EntityManager em = PersistenceManager.getInstance().createEntityManager();
+			em.getTransaction().begin();
+			em.getTransaction().commit();
+			em.close();
+		}catch(Exception e) {
+			fail("Could not open connection");
+		}
+	}
+
+}
